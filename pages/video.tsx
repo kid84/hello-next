3cfb9bc2edd255c80104f734554a7bc74e937bad
Hashikawa Takuya <hashikawa.takuya@gmail.com>
2022-08-30 15:18:58 +0900

    video control

--- pages/video.tsx
+++ pages/video.tsx
@@ -1,9 +1,16 @@
 import {NextPage} from "next";
+import {useEffect} from "react";
 
 const VideoPage: NextPage = () => {
+  useEffect(() => {
+    // @ts-ignore
+    const video:HTMLVideoElement | null = document.getElementById("video");
+    if (!video) return;
+    video.play()
+  },[])
   return(
     <div>
-      <video muted src="/movies/atrae_opening.mp4" autoPlay loop />
+      <video id="video" muted src="/movies/atrae_opening.mp4" loop />
     </div>
   )
 }
