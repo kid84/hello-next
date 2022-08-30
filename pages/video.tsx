e2a4c9ea4c7e4ca9e0d921edab5611f4b86db9eb
Hashikawa Takuya <hashikawa.takuya@gmail.com>
2022-08-30 15:41:30 +0900

    remove useEffect

--- pages/video.tsx
+++ pages/video.tsx
@@ -2,18 +2,18 @@ import {NextPage} from "next";
 import {useEffect} from "react";
 
 const VideoPage: NextPage = () => {
-  useEffect(() => {
-    window.addEventListener("load", () => {
-      // @ts-ignore
-      const video:HTMLVideoElement | null = document.getElementById("video");
-      if (!video) return;
-      video.play()
-    });
-  },[])
+  // useEffect(() => {
+  //   window.addEventListener("load", () => {
+  //     // @ts-ignore
+  //     const video:HTMLVideoElement | null = document.getElementById("video");
+  //     if (!video) return;
+  //     video.play()
+  //   });
+  // },[])
 
   return(
     <div>
-      <h1>playsinline="" autoplay=""</h1>
+      <h1>remove useEffect</h1>
       {/*// @ts-ignore*/}
       <video id="video" playsinline="" autoplay="" muted src="/movies/atrae_opening.mp4" loop />
     </div>
